8aae446e1c17b46bc4a358b218b352cd40ee5943
bigreni <bigreni@gmail.com>
2017-04-05 13:54:20 -0500

    Version 74

--- www/js/admob.js
+++ www/js/admob.js
@@ -44,72 +44,26 @@
         }
     // optional, in case respond to events or handle error
     function registerAdEvents() {
-        /* deprecated
-        document.addEventListener('onBannerFailedToReceive', function(data){ alert('error: ' + data.error + ', reason: ' + data.reason); });
-        document.addEventListener('onBannerReceive', function(){});
-        document.addEventListener('onBannerPresent', function(){});
-        document.addEventListener('onBannerLeaveApp', function(){});
-        document.addEventListener('onBannerDismiss', function(){});
-
-        document.addEventListener('onInterstitialFailedToReceive', function(data){ alert('error: ' + data.error + ', reason: ' + data.reason); });
-        document.addEventListener('onInterstitialReceive', function(){});
-        document.addEventListener('onInterstitialPresent', function(){});
-        document.addEventListener('onInterstitialLeaveApp', function(){});
-        document.addEventListener('onInterstitialDismiss', function(){});
-        */
 
         // new events, with variable to differentiate: adNetwork, adType, adEvent
         document.addEventListener('onAdFailLoad', function (data) {
-            document.getElementById('main').style.visibility = 'visible';
+            document.getElementById('fullpage').style.display = 'block';
+            //document.getElementById('main').style.visibility = 'visible';
             document.getElementById('screen').style.display = 'none';
         });
         document.addEventListener('onAdLoaded', function (data) { });
         document.addEventListener('onAdPresent', function (data) { });
         document.addEventListener('onAdLeaveApp', function (data) { });
         document.addEventListener('onAdDismiss', function (data) {
-            document.getElementById('main').style.visibility = 'visible';
+            document.getElementById('fullpage').style.display = 'block';
+            //document.getElementById('main').style.visibility = 'visible';
             document.getElementById('screen').style.display = 'none';
 			});
     }
 
-    // click button to call following functions
-    //function getSelectedAdSize() {
-    //    var i = document.getElementById("adSize").selectedIndex;
-    //    var items = document.getElementById("adSize").options;
-    //    return items[i].value;
-    //}
-    //function getSelectedPosition() {
-    //    var i = document.getElementById("adPosition").selectedIndex;
-    //    var items = document.getElementById("adPosition").options;
-    //    return parseInt(items[i].value);
-    //}
     function createSelectedBanner() {
-        //var overlap = document.getElementById('overlap').checked;
-        //var offsetTopBar = document.getElementById('offsetTopBar').checked;
-        //AdMob.createBanner( {adId:admobid.banner, overlap:overlap, offsetTopBar:offsetTopBar, adSize: getSelectedAdSize(), position:getSelectedPosition()} );
-        AdMob.createBanner({adId:admobid.banner});
+       AdMob.createBanner({adId:admobid.banner});
     }
-    //function createBannerOfGivenSize() {
-    //    var w = document.getElementById('w').value;
-    //    var h = document.getElementById('h').value;
-
-    //    AdMob.createBanner({ adId: admobid.banner,
-    //        adSize: 'CUSTOM', width: w, height: h,
-    //        position: getSelectedPosition()
-    //    });
-    //}
-    //function showBannerAtSelectedPosition() {
-    //    AdMob.showBanner(getSelectedPosition());
-    //}
-    //function showBannerAtGivenXY() {
-    //    var x = document.getElementById('x').value;
-    //    var y = document.getElementById('y').value;
-    //    AdMob.showBannerAtXY(x, y);
-    //}
-    //function prepareInterstitial() {
-    //    var autoshow = document.getElementById('autoshow').checked;
-    //    AdMob.prepareInterstitial({ adId: admobid.interstitial, autoShow: autoshow });
-    //}
 
     function successFunction()
     {
@@ -127,9 +81,12 @@
    function checkFirstUse()
     {
 
-           askRating();
-           initApp();
+           //askRating();
+           //initApp();
 		   //onDeviceReady();
+            document.getElementById('fullpage').style.display = 'block';
+            document.getElementById('screen').style.display = 'none';
+
     }
 
 function askRating()
